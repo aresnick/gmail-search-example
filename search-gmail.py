1524e220376a3c86ee2d5f5c4118b79dd94ef763
Alec Resnick <aresnick@Alecs-MacBook-Pro.local>
2018-10-06 14:52:47 -0400

    Adding search, logging

--- search-gmail.py
+++ search-gmail.py
@@ -4,20 +4,32 @@ from httplib2 import Http
 from apiclient import discovery
 from oauth2client import file, client, tools
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
 config = {
+	'service': None,
 	'user': 'me',
 	'query': 'from:me',
 	'maxResults': 1
 }
 
 def authenticate():
+	logging.debug("Beginning authentication…")
 	# Set the scope we'll be using in our search; since we're only searching we only need readonly
 	SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
 	# Grab our stored credentials, running the OAuth flow if we don't have them already
+	logging.debug("Looking for local authentication information…")
 	store = file.Storage('storage.json') 
 	creds = store.get()
 	if not creds or creds.invalid:
+		if not cred:
+			logging.debug("Local credentials not found.")
+		elif creds.invalid:
+			logging.debug("Local credentials not valid.")
+		logging.debug("Loading authentication flow…")
 		flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
 		creds = tools.run_flow(flow, store)
 
@@ -25,4 +37,17 @@ def authenticate():
 	GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
 	return GMAIL
 
-service = authenticate()
+config.service = authenticate()
+
+def search(query=config.query, maxResults=config.maxResults, user=config.user, service=config.service):
+	logging.debug(" ".join([
+		"Beginning search for",
+		query + ',',
+		"limiting results to",
+		maxResults,
+		"as",
+		user + '…'
+		]))
+	response = GMAIL.users().messages().list(userId=user, maxResults=maxResults, q=query).execute()
+	return response
+

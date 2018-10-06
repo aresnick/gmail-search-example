1a9381a9e6583fc10a4980e783dc7430a5533e30
Alec Resnick <aresnick@Alecs-MacBook-Pro.local>
2018-10-06 15:23:17 -0400

    Refactoring as a class to avoid constantly passing around dependencies

--- search-gmail.py
+++ search-gmail.py
@@ -5,7 +5,7 @@ from apiclient import discovery
 from oauth2client import file, client, tools
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 
 
 config = {
@@ -15,38 +15,53 @@ config = {
 	'maxResults': 1
 }
 
-def authenticate():
-	logging.info("Beginning authentication...")
-	# Set the scope we'll be using in our search; since we're only searching we only need readonly
-	SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+class GmailSearch:
+	def __init__(self, user='me', query='from:me', maxResults=1):
+		self.user = user
+		self.query = query
+		self.maxResults = maxResults
 
-	# Grab our stored credentials, running the OAuth flow if we don't have them already
-	logging.info("Looking for local authentication information...")
-	store = file.Storage('storage.json') 
-	creds = store.get()
-	if not creds or creds.invalid:
-		if not cred:
-			logging.error("Local credentials not found.")
-		elif creds.invalid:
-			logging.error("Local credentials not valid.")
-		logging.info("Loading authentication flow...")
-		flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-		creds = tools.run_flow(flow, store)
+		self.service = self.authenticate()
+		self.results = []
 
-	# Create the GMAIL service object with our credentials
-	GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
-	return GMAIL
+	def authenticate(self):
+		logging.info("Beginning authentication...")
+		# Set the scope we'll be using in our search; since we're only searching we only need readonly
+		SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
-config['service'] = authenticate()
+		# Grab our stored credentials, running the OAuth flow if we don't have them already
+		logging.info("Looking for local authentication information...")
+		store = file.Storage('storage.json') 
+		creds = store.get()
+		if not creds or creds.invalid:
+			if not cred:
+				logging.error("Local credentials not found.")
+			elif creds.invalid:
+				logging.error("Local credentials not valid.")
+			logging.info("Loading authentication flow...")
+			flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+			creds = tools.run_flow(flow, store)
 
-def search(query=config['query'], maxResults=config['maxResults'], user=config['user'], service=config['service']):
-	logging.info(" ".join([
-		"Beginning search for",
-		query + ',',
-		"limiting results to",
-		maxResults,
-		"as",
-		user + '...'
-		]))
-	response = GMAIL.users().messages().list(userId=user, maxResults=maxResults, q=query).execute()
-	return response
+		# Create the GMAIL service object with our credentials
+		GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
+		return GMAIL
+
+	def search(self):
+		logging.info(" ".join(["Beginning search for", self.query + ',', "limiting results to", str(self.maxResults), "as", self.user + '...' ]))
+
+		response = self.service.users().messages().list(userId=self.user, maxResults=self.maxResults, q=self.query).execute()
+		if 'messages' in response:
+			logging.debug(" ".join(["Received", str(len(response['messages'])), "messages..."]))
+			self.results.extend(response['messages'])
+		else:
+			logging.info("No messages received.")
+
+		while (len(self.results) < self.maxResults) and ('nextPageToken' in response):
+			logging.debug("Retrieving next page of messages")
+			page_token = response['nextPageToken']
+			response = self.service.users().messages().list(userId=self.user, maxResults=self.maxResults, q=self.query, pageToken=page_token).execute()
+			self.results.extend(response['messages'])
+
+		logging.info(" ".join(["Found a total of", str(len(self.results)), "messages"]))
+
+		return self.results
